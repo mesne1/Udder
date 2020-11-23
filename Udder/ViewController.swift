be61bef5ca2d270ed765e1747533e2ae78296040
peter <123@abc.com>
2020-11-24 00:38:07 +0100

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by SGMobile TestDev on 24/11/2020.
+//  Copyright © 2020 SGMobile TestDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
